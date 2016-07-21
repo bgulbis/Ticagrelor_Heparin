23f040122ca18cd26b3868a98927ef0518fcf4c1
bgulbis <brian.gulbis@gmail.com>
2016-07-21 11:26:52 -0500

    ticag and warfarin overlap

--- 1-patients.R
+++ 1-patients.R
@@ -17,9 +17,9 @@ concat_encounters(raw.patients$pie.id)
 raw.meds_sched <- read_data(data.raw, "meds_sched") %>%
     as.meds_sched()
 
-ref <- data_frame(name = c("heparin", "ticagrelor"),
+ref <- data_frame(name = c("heparin", "ticagrelor", "warfarin"),
                   type = "med",
-                  group = c("cont", "sched"))
+                  group = c("cont", "sched", "sched"))
 
 tmp.heparin <- read_data(data.raw, "meds_cont") %>%
     as.meds_cont() %>%
@@ -28,16 +28,40 @@ tmp.heparin <- read_data(data.raw, "meds_cont") %>%
     summarize_data() %>%
     rename(heparin = med)
 
-tmp.ticag <- tidy_data(raw.meds_sched, ref = ref) %>%
+tmp.meds_sched <- tidy_data(raw.meds_sched, ref = ref)
+
+tmp.ticag <- tmp.meds_sched %>%
+    filter(med == "ticagrelor") %>%
     rename(ticag = med)
 
-data.overlap <- left_join(tmp.ticag, tmp.heparin, by = "pie.id") %>%
-    mutate(concur = if_else(med.datetime >= start.datetime &
-                                med.datetime <= stop.datetime,
-                            TRUE, FALSE, FALSE)) %>%
+tmp.warf <- tmp.meds_sched %>%
+    filter(med == "warfarin") %>%
     group_by(pie.id) %>%
-    summarize(concur = sum(concur))
+    arrange(pie.id, med.datetime) %>%
+    summarize(warf.start = first(med.datetime),
+              warf.stop = last(med.datetime),
+              num.warf = n())
 
-data.patients <- filter(data.overlap, concur > 0)
+data.overlap <- left_join(tmp.ticag, tmp.heparin, by = "pie.id") %>%
+    left_join(tmp.warf, by = "pie.id") %>%
+    mutate(tic_hep = if_else(med.datetime >= start.datetime &
+                                med.datetime <= stop.datetime,
+                            TRUE, FALSE, FALSE),
+           tic_war = if_else(med.datetime >= warf.start &
+                                 med.datetime <= warf.stop,
+                             TRUE, FALSE, FALSE),
+           war_hep = int_overlaps(interval(start.datetime, stop.datetime, "US/Central"),
+                                  interval(warf.start, warf.stop, "US/Central"))) %>%
+    group_by(pie.id) %>%
+    summarize(tic_hep = sum(tic_hep),
+              tic_war = sum(tic_war),
+              war_hep = sum(war_hep))
+
+data.patients <- filter(data.overlap, tic_hep > 0)
+
+smry <- data.overlap %>%
+    summarize(num_patients = n(),
+              num_any_overlap = sum(tic_hep > 0),
+              num_3days_overlap = sum(tic_hep >= 6))
 
 readr::write_csv(data.overlap, "data-tidy/overlap.csv")
